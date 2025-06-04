81998cb4805fcb0d189eece4b2bc869d3a528656
SpandanBG <spandanbg@gmail.com>
2025-06-04 17:34:03 +0530

    update root view to flex and add log box and prompt input

--- ui/layout.go
+++ ui/layout.go
@@ -3,7 +3,8 @@ package ui
 import "github.com/rivo/tview"
 
 const (
-	appTitle = "<Ctrl+LOG>"
+	appTitle   = "<Ctrl+LOG>"
+	inputLabel = ":"
 )
 
 type App interface {
@@ -11,18 +12,21 @@ type App interface {
 }
 
 type app struct {
-	tui *tview.Application
+	tui    *tview.Application
+	root   *tview.Flex
+	logBox *tview.Box
+	prompt *tview.InputField
 }
 
 // New - creates a new logctr app using tview package.
 func New() App {
-	tui := tview.NewApplication()
+	app := &app{tui: tview.NewApplication()}
 
-	createRoot(tui)
+	app.createRoot()
+	app.createLogBox()
+	app.createPrompt()
 
-	return &app{
-		tui: tui,
-	}
+	return app
 }
 
 // Run - starts the app. panics with an error if unable to run
@@ -34,9 +38,20 @@ func (a *app) Run() {
 
 // --------------------- private methods and function
 
-// createRoot - creates a fullscreen box window with the app title at top as
-// the root of the viewport.
-func createRoot(app *tview.Application) {
-	box := tview.NewBox().SetBorder(true).SetTitle(appTitle)
-	app.SetRoot(box, true)
+// createRoot - creates a fullscreen flex window as the root of the viewport.
+func (a *app) createRoot() {
+	a.root = tview.NewFlex().SetDirection(tview.FlexRow)
+	a.tui.SetRoot(a.root, true).EnableMouse(true)
+}
+
+// createLogBox - creates a box with app title at the top as log dialog box.
+func (a *app) createLogBox() {
+	a.logBox = tview.NewBox().SetBorder(true).SetTitle(appTitle)
+	a.root.AddItem(a.logBox, 0, 1, false)
+}
+
+// createPrompt - creates an input field with focused for user inputs.
+func (a *app) createPrompt() {
+	a.prompt = tview.NewInputField().SetLabel(inputLabel)
+	a.root.AddItem(a.prompt, 1, 0, true)
 }

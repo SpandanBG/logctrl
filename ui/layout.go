5f89d0d7a931d915919644d56c7d92531821dc87
SpandanBG <spandanbg@gmail.com>
2025-06-24 17:02:16 +0530

    move log view to root level in layout

--- ui/layout.go
+++ ui/layout.go
@@ -36,9 +36,8 @@ type logTeaCmd string
 
 type uiModel struct {
 	toolbar   tea.Model
+	logView   tea.Model
 	currentFG focusGroup
-	logFG     []tea.Model
-	helpFG    []tea.Model
 }
 
 func NewUI(stream reader.Stream) (
@@ -48,10 +47,8 @@ func NewUI(stream reader.Stream) (
 	app = tea.NewProgram(
 		uiModel{
 			toolbar:   components.NewToolbar(ui.SizeRatio(1), ui.SizeFixed(toolbarSize)),
+			logView:   components.NewLogView(ui.SizeRatio(1), ui.SizeModifier(-toolbarSize), stream),
 			currentFG: LOG_FG,
-			logFG: []tea.Model{
-				components.NewLogView(ui.SizeRatio(1), ui.SizeModifier(-toolbarSize), stream),
-			},
 		},
 		tea.WithAltScreen(),
 	)
@@ -65,23 +62,11 @@ func NewUI(stream reader.Stream) (
 }
 
 func (u uiModel) Init() tea.Cmd {
-	var fg []tea.Model
 
-	switch u.currentFG {
-	case LOG_FG:
-		fg = u.logFG
-	case HELP_FG:
-		fg = u.helpFG
-	default:
-		return nil
-	}
-
-	var cmds []tea.Cmd
-	for _, each := range fg {
-		cmds = append(cmds, each.Init())
-	}
-
-	return tea.Batch(cmds...)
+	return tea.Batch(
+		u.toolbar.Init(),
+		u.logView.Init(),
+	)
 }
 
 func (u uiModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
@@ -90,37 +75,18 @@ func (u uiModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		return u.executeKeystroke(msg.String())
 	}
 
-	cmds := []tea.Cmd{nil}
+	cmds := make([]tea.Cmd, 2)
 	u.toolbar, cmds[0] = u.toolbar.Update(msg)
-
-	switch u.currentFG {
-	case LOG_FG:
-		u.logFG, cmds = u.batchUpdate(msg, u.logFG)
-	case HELP_FG:
-		u.helpFG, cmds = u.batchUpdate(msg, u.helpFG)
-	default:
-		return u, nil
-	}
+	u.logView, cmds[1] = u.logView.Update(msg)
 
 	return u, tea.Batch(cmds...)
 }
 
 func (u uiModel) View() string {
-	var fg []tea.Model
-	switch u.currentFG {
-	case LOG_FG:
-		fg = u.logFG
-	case HELP_FG:
-		fg = u.helpFG
-	default:
-		return ""
-	}
+	views := make([]string, 2)
 
-	views := make([]string, len(fg)+1)
 	views[0] = u.toolbar.View()
-	for i, each := range fg {
-		views[i+1] = each.View()
-	}
+	views[1] = u.logView.View()
 
 	return strings.Join(views, "\n")
 }
@@ -130,34 +96,7 @@ func (u uiModel) executeKeystroke(key string) (tea.Model, tea.Cmd) {
 	switch key {
 	case "ctrl+c", "ctrl+d", "q":
 		return u, tea.Quit
-	case "tab":
-		return u.nextFocusGroup()
 	default:
 		return u, nil
 	}
-}
-
-func (u uiModel) batchUpdate(msg tea.Msg, fg []tea.Model) ([]tea.Model, []tea.Cmd) {
-	cmds := make([]tea.Cmd, len(fg))
-	for i, each := range fg {
-		fg[i], cmds[i] = each.Update(msg)
-	}
-	return fg, cmds
-}
-
-func (u uiModel) nextFocusGroup() (tea.Model, tea.Cmd) {
-	// Go to next focus group
-	u.currentFG = (u.currentFG + NEXT_UNIT) % LOOPBACK
-
-	var cmds []tea.Cmd
-	switch u.currentFG {
-	case LOG_FG:
-		u.logFG, cmds = u.batchUpdate(nil, u.logFG)
-	case HELP_FG:
-		u.helpFG, cmds = u.batchUpdate(nil, u.helpFG)
-	default:
-		return u, nil
-	}
-
-	return u, tea.Batch(cmds...)
 }

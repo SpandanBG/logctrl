cd7fb81a5fd415886936ea17bd1b5c99d6e82d5d
SpandanBG <spandanbg@gmail.com>
2025-07-28 22:44:46 +0530

    add buffer re-size functionality

--- reader/buffer.go
+++ reader/buffer.go
@@ -4,6 +4,7 @@ import "strings"
 
 type Buffer interface {
 	Stringify(string) string
+	Resize(int)
 	Push(string)
 }
 
@@ -40,6 +41,15 @@ func (b *buffer) Push(msg string) {
 	}
 }
 
+// Resize - updates and size of the buffer and makes sure the old data is present
+func (b *buffer) Resize(newSize int) {
+	if b.size > newSize {
+		b.shrink(newSize)
+	} else {
+		b.enlarge(newSize)
+	}
+}
+
 // Stringify - joins the data in the buffer into a single string with the
 // provided separator.
 func (b *buffer) Stringify(separator string) string {
@@ -47,6 +57,56 @@ func (b *buffer) Stringify(separator string) string {
 		return ""
 	}
 
+	final := make([]string, b.filled)
+	b.forEach(func(s string, i int) bool {
+		final[i] = s
+		return true
+	})
+
+	return strings.Join(final, separator)
+}
+
+// ----------------------- PRIVATE
+
+// shrink - reduces the size of the buffer and keeps old data
+// TODO - optimize iteration of old buffer to copy to new buffer instead of
+// copying from start to end, we can just try and copy from the items that would
+// be left in the final buffer.
+func (b *buffer) shrink(newSize int) {
+	if b.size < newSize {
+		return
+	}
+
+	nb := NewBuffer(newSize)
+
+	b.forEach(func(s string, _ int) bool {
+		nb.Push(s)
+		return true
+	})
+
+	copy(nb.(*buffer), b)
+}
+
+// enlarge - increases the size of the buffer and keeps old data
+func (b *buffer) enlarge(newSize int) {
+	if b.size > newSize {
+		return
+	}
+
+	nb := NewBuffer(newSize)
+
+	b.forEach(func(s string, _ int) bool {
+		nb.Push(s)
+		return true
+	})
+
+	copy(nb.(*buffer), b)
+}
+
+// forEach - Iterates through each item in the buffer and calls the passed
+// callback with the value and index. If callback returns `false`, the iteration
+// will be stopped at that point.
+func (b *buffer) forEach(act func(string, int) bool) {
 	x := b.i
 	if b.filled == b.size {
 		x -= 1
@@ -55,10 +115,18 @@ func (b *buffer) Stringify(separator string) string {
 		}
 	}
 
-	final := make([]string, b.filled)
 	for i := 0; i < b.filled; i += 1 {
-		final[i] = b.buf[(x+i)%b.size]
+		if ok := act(b.buf[(x+i)%b.size], i); !ok {
+			break
+		}
 	}
+}
 
-	return strings.Join(final, separator)
+// copy - copies one buffer items to another
+func copy(from, to *buffer) {
+	to.buf = from.buf
+	to.size = from.size
+	to.filled = from.filled
+	to.i = from.i
+	to.j = from.j
 }
--- reader/buffer_test.go
+++ reader/buffer_test.go
@@ -12,7 +12,7 @@ func Equal[T comparable](t *testing.T, expected, actual T) {
 	)
 }
 
-func TestBufferTableDriven(t *testing.T) {
+func Test_BufferTableDriven(t *testing.T) {
 	for _, test := range []struct {
 		name     string
 		size     int
@@ -63,12 +63,101 @@ func TestBufferTableDriven(t *testing.T) {
 	}
 }
 
-func BenchmarkBuffer(b *testing.B) {
-	buf := NewBuffer(2)
+func Test_BufferResizeTableDriven(t *testing.T) {
+	for _, test := range []struct {
+		name       string
+		size       int
+		resize     int
+		push       []string
+		expected   string
+		rePush     []string
+		reExpected string
+	}{
+		{
+			name:       "enlarge from 1 to 2 - full",
+			size:       1,
+			resize:     2,
+			push:       []string{"a"},
+			expected:   "a",
+			rePush:     []string{"b"},
+			reExpected: "a\nb",
+		},
+		{
+			name:       "enlarge from 2 to 4 - not full",
+			size:       2,
+			resize:     4,
+			push:       []string{"a"},
+			expected:   "a",
+			rePush:     []string{"b", "c"},
+			reExpected: "a\nb\nc",
+		},
+		{
+			name:       "enlarge from 2 to 4 - overfill",
+			size:       2,
+			resize:     4,
+			push:       []string{"a", "b", "c"},
+			expected:   "b\nc",
+			rePush:     []string{"d", "e"},
+			reExpected: "b\nc\nd\ne",
+		},
+		{
+			name:       "shrink from 4 to 2 - not full",
+			size:       4,
+			resize:     2,
+			push:       []string{"a"},
+			expected:   "a",
+			rePush:     []string{"b"},
+			reExpected: "a\nb",
+		},
+		{
+			name:       "shrink from 4 to 2 - full",
+			size:       4,
+			resize:     2,
+			push:       []string{"a", "b", "c", "d"},
+			expected:   "a\nb\nc\nd",
+			rePush:     []string{"e"},
+			reExpected: "d\ne",
+		},
+		{
+			name:       "shrink from 4 to 2 - overfill",
+			size:       4,
+			resize:     2,
+			push:       []string{"a", "b", "c", "d", "e"},
+			expected:   "b\nc\nd\ne",
+			rePush:     []string{"f"},
+			reExpected: "e\nf",
+		},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			buf := NewBuffer(test.size)
+
+			// -------- pre-resize
+			for _, each := range test.push {
+				buf.Push(each)
+			}
+
+			str := buf.Stringify("\n")
+			Equal(t, test.expected, str)
+
+			// -------- post-resize
+			buf.Resize(test.resize)
+			for _, each := range test.rePush {
+				buf.Push(each)
+			}
+
+			str = buf.Stringify("\n")
+			Equal(t, test.reExpected, str)
+		})
+	}
+}
+
+func Benchmark_Buffer(b *testing.B) {
+	buf := NewBuffer(4)
 
 	for _, each := range []string{"a", "b", "c", "d"} {
 		buf.Push(each)
 	}
 
+	buf.Resize(2)
 	_ = buf.Stringify("\n")
 }
